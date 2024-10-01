63949fc0547a94a2f0cddc44c28f676e3118d8f8
Elisei Mankov <eliseimankov@gmail.com>
2024-10-01 11:51:35 +0300

    Added constant for conversion from atomic frequency to attoseconds

--- fortran_output_analysis/constants_and_parameters.py
+++ fortran_output_analysis/constants_and_parameters.py
@@ -1,13 +1,22 @@
 # Photon/energy
 g_eV_per_Hartree = 27.211386245988
-g_omega_IR = 0.0569614*g_eV_per_Hartree
+g_omega_IR = 0.0569614 * g_eV_per_Hartree
 
 # Plot parameters
 g_linewidth = 2
 g_fontsize = 20
-g_colors_list = ['royalblue', 'crimson', 'lawngreen', 'violet', 'orange', 'black', 'deepskyblue', 'pink', 'lightseagreen']
-g_linestyles = ["solid",
-                "dotted",
-                "dashdot",
-                "dashed",
-                (0, (3, 1, 1, 1, 1, 1))]
+g_colors_list = [
+    "royalblue",
+    "crimson",
+    "lawngreen",
+    "violet",
+    "orange",
+    "black",
+    "deepskyblue",
+    "pink",
+    "lightseagreen",
+]
+g_linestyles = ["solid", "dotted", "dashdot", "dashed", (0, (3, 1, 1, 1, 1, 1))]
+
+# Conversion constants
+g_inverse_atomic_frequency_to_attoseconds = 1e18 * 2.4188843265e-17
